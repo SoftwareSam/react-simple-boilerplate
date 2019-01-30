28a91c12b87b3f5f0f56961c03f6d7c2e76849de
Sam Soheylee <ssoheyleearasi@gmail.com>
2019-01-30 19:37:13 +0000

    updated state correctly to post message

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,11 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+    this.updateMessageState = this.updateMessageState.bind(this)
     this.state = {
 
-    currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
+    currentUser: {name: "Bob"},
+    messages: [
     {
       username: "Bob",
       content: "Has anyone seen my marbles?",
@@ -25,7 +26,26 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
+  updateMessageState(messageValue){
+    this.setState({messages: [...this.state.messages, {username: "Bob", content: messageValue, id: }]})
+  }
+ }
+
   render() {
+    console.log(this.state.messages);
     return (
       <div>
         <nav className="navbar">
@@ -33,7 +53,9 @@ class App extends Component {
         </nav>
         <MessageList messages={this.state.messages}/>
         <SysMsg />
-        <ChatBar currentUser={this.state.currentUser}/>
+        <ChatBar currentUser={this.state.currentUser}
+                 handleChangeFunc={this.updateMessageState}
+        />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,12 +2,27 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class ChatBar extends Component {
+  handleKeyPress = (event) => {
+    if(event.key == 'Enter'){
+      this.handleMessage(event)
+    }
+  }
+
+  handleMessage = (event) => {
+    console.log(event.target.value)
+    let newMessage = event.target.value
+    this.props.handleChangeFunc(newMessage)
+  }
 
   render(){
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          onKeyPress={this.handleKeyPress}
+        />
       </footer>
     );
   }
