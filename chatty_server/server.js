87e4ddabe73ccac2f00ab76339edff78fbe4e72b
Sam Soheylee <ssoheyleearasi@gmail.com>
2019-01-31 03:18:52 +0000

    Able to send messages to server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -21,6 +21,14 @@ const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
+  // when message is received, do thing in brackets
+  ws.on('message', (message) => {
+
+    let parsedMess = JSON.parse(message);
+    console.log(parsedMess.content);
+
+
+  })
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,25 @@ class App extends Component {
     };
   }
 
+
+
+
+
   componentDidMount() {
+
     // this is the socket connection object
+    // opened
     this.socket = new WebSocket("ws://localhost:3001/");
+
+    // listen for incoming messages
+    this.socket.onmessage = function (event) {
+      console.log(event.data);
+    }
+
+
 
     console.log("componentDidMount <App />");
     setTimeout(() => {
-      console.log("Simulating incoming message");
     // Add a new message to the list of messages in the data store
     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
     const messages = this.state.messages.concat(newMessage)
@@ -44,12 +56,15 @@ class App extends Component {
   }
 
   updateMessageState(messageValue){
-    this.setState({messages: [...this.state.messages, {username: this.state.currentUser.name, content: messageValue}] })
+    let newMess = {username: this.state.currentUser.name, content: messageValue}
+    // this.setState({messages: [...this.state.messages, {username: this.state.currentUser.name, content: messageValue}] })
+    let newMessStringify = JSON.stringify(newMess);
+    this.socket.send(newMessStringify);
+
   }
 
   updateUserState(userValue){
     this.setState({currentUser: {name: userValue}})
-    console.log(this.state)
   }
 
 
